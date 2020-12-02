b7b6b756b842e57911aa77d276a3e647db97aabc
Ia Lundell <olivia.solumlundell@gmail.com>
2020-12-02 21:56:37 +0100

    Add project files.

---
+++ AdventofCode1/AdventofCode1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventofCode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uppgift: Hitta två tal i listan som tillsammans blir 2020. Multiplicera båda talen och ange summan i AdventOfCode
+            var inputData = File.ReadAllLines(@"C:\Users\olivi\source\repos\AdventofCode1\AdventofCode1\adventofcode.txt");
+            int[] numArr = Enumerable.Range(0, inputData.Length).Select(i => int.Parse(inputData[i].Trim())).ToArray();
+
+            foreach (var num1 in numArr)
+            {
+                foreach (var num2 in numArr)
+                {
+                    foreach (var num3 in numArr)
+                    {
+                        if (num1 + num2 + num3 == 2020 && 
+                            num1 != num2 && num1 != num3 && num2 != num3)
+                        {
+                            int total = num1 * num2 * num3;
+                            Console.WriteLine(total);
+                            Console.ReadKey();
+                            return;
+                           
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AdventofCode2/AdventofCode2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace AdventofCode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputData = File.ReadAllLines(@"C:\Users\olivi\source\repos\AdventofCode2\Adventofcode2.txt");
+            int CorrectPasswords = 0;
+
+            foreach (var input in inputData)
+            {
+                if (isValid(input) == true)
+                {
+                    CorrectPasswords++;
+                }
+            }
+            Console.WriteLine(CorrectPasswords);
+            Console.ReadKey();
+        }
+        public static bool isValid(string inputData)
+        {
+            string[] split1 = inputData.Split(' ');
+            if (split1.Length != 3) throw new FormatException("Invalid input");
+
+            var numSpan = split1[0].Split('-');
+            (var min, var max) = (int.Parse(numSpan[0]), int.Parse(numSpan[1]));
+
+            var forcedLetter = split1[1].Replace(":", "");
+
+            int letters = 0;
+            foreach (var letter in split1[2])
+            {
+                if (letter.ToString() == forcedLetter)
+                {
+                    letters++;
+                }
+            }
+            return ((split1[2].IndexOf(forcedLetter, min - 1) == min - 1 || split1[2].IndexOf(forcedLetter, max - 1) == max - 1)) &&
+                   !((split1[2].IndexOf(forcedLetter, min - 1) == min - 1 && split1[2].IndexOf(forcedLetter, max - 1) == max - 1));
+        }
+    }
+}
+
